fa45301a7052f38896d278b1c19b6a5cb14338c4
Evgeniy Koncov <diab@bk.ru>
2023-09-20 14:12:36 +0300

    Продолжаем изучение главы 15.

--- Глава 15/Упражнение15-3.py
+++ Глава 15/Упражнение15-3.py
@@ -6,14 +6,14 @@ from random_walk import RandomWalk
 # Новые блуждания строятся до тех пор, пока программа остается активной.
 while True:
     # Построение случайного блуждания.
-    rw = RandomWalk(50000)
+    rw = RandomWalk(5000)
     rw.fill_walk()
 
     # Нанесение точек на диаграмму.
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+    plt.plot(rw.x_values, rw.y_values, linewidth=True)
 
     # Выделение первой и последней точек.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
